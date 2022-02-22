9a70061075762331837e985b690c056671dfed9e
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-22 22:22:13 +0800

    feat: dynamic css class

--- src/components/VehicleImage/VehicleImage.js
+++ src/components/VehicleImage/VehicleImage.js
@@ -5,7 +5,7 @@ export default class VehicleImage extends Component {
   render() {
     const { src, name, location, alt, className } = this.props
     return (
-      <div className={`${style.wrapper} position-relative col`}>
+      <div className={`${style.wrapper} ${className} position-relative`}>
         <img className={style.img} src={src} alt={alt || ''} srcSet="" />
         <div className={`${style.desc} position-absolute`}>
           <h3 className={style.name}>{name}</h3>
