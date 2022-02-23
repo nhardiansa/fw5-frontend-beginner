5a23a8c5945c7a4ae1789b611a8b858dd18ca497
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-23 08:59:46 +0800

    feat: Layout Component

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Footer from './Footer/Footer'
+import Navbar from './Navbar'
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <>
+        <Navbar isLogged={this.props.isLogged} />
+          {this.props.children}
+        <Footer />
+      </>
+    )
+  }
+}
