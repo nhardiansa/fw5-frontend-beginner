1a93fbd9b795025848127df7792eec34f38aa9f1
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-24 22:18:19 +0800

    fix: fetch data by params type

--- src/pages/ViewMore/index.js
+++ src/pages/ViewMore/index.js
@@ -1,52 +1,101 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import VehicleImage from '../../components/VehicleImage/VehicleImage'
 
-import pict from '../../assets/img/popular/eric-muhr.png'
 import Layout from '../../components/Layout'
+import { getData } from '../../helpers/http'
+import { capitalize } from '../../helpers/stringFormat'
 
 import './style.css'
 
-export default class ViewMore extends Component {
-  render() {
-    return (
-      <Layout isLogged={true}>
-        <main>
-          <div className="view-more-wrapper container">
+export const ViewMore = () =>  {
+  const {type} = useParams()
+  const [vehicles, setVehicle] = useState([])
+  // const [category, setCategory] = useState([])
+
+  useEffect(() => {
+    if (vehicles.length === 0) {
+      mapVehicleData(type)
+    }
+  }, [])
+
+  const mapVehicleData = (type) => {
+    switch (type) {
+      case 'motorbike':
+        getVehicle('/vehicles/filter?limit=16&category_id=3', setVehicle)
+        break;
+      case 'car':
+        getVehicle('/vehicles/filter?limit=16&category_id=2', setVehicle)
+        break;
+      case 'bike':
+        getVehicle('/vehicles/filter?limit=16&category_id=4', setVehicle)
+        break;
+      default:
+        getVehicle('/vehicles/popular', setVehicle)
+        break;
+    }
+  }
+
+  const getVehicle = async (uri, stateReducer) => {
+    try {
+      const data = await getData(uri)
+      stateReducer(data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getCategory = async (uri, stateReducer) => {
+    try {
+      const categories = await getData(uri)
+      stateReducer(categories.results)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  return (
+    <Layout isLogged={true}>
+      <main className={vehicles.length === 0 ? 'vh-100' : ''}>
+        <div className={`view-more-wrapper container d-flex flex-column`}>
+          {
+            type === 'popular' &&
             <h1 className="view-more-title text-center text-md-start">Popular in town</h1>
+          }
+          {
+            type !== 'popular' &&
+            <h1 className="view-more-title text-center text-md-start">{capitalize(type) + 's'}</h1>
+          }
+          {
+            vehicles.length > 0 && 
             <p className="click-suggestion mt-4 text-center fw-bold">
               Click item to see details and reservation
             </p>
-            <div
-              className="view-more-container mt-5 d-flex flex-column align-items-center flex-md-row flex-wrap"
-            >
-              {
-                [...Array(12).fill(1)].map((el, i) => (
-                  <VehicleImage
-                    key={i}
-                    src={pict}
-                    name='Merapi'
-                    location='Yogyakarta'
-                    className='view-more-item h-75 p-0 me-md-3 col-12 col-md'
-                  />
-                  // <a key={i} href="/vehicle-detail.html" className="view-more-item mb-4 me-md-4 position-relative">
-                  //   <img
-                  //     src={pict}
-                  //     alt="white-jeep"
-                  //     className="item-img w-100"
-                  //   />
-                  //   <div className="item-desc position-absolute">
-                  //     <p className="item-name fw-bold">Merapi</p>
-                  //     <p className="item-location">Yogyakarta</p>
-                  //   </div>
-                  // </a>
-                ))
-              }
-            </div>
-            <p className="no-items mt-5 text-center">There is no vehicle left</p>
+          }
+          <div
+            className="view-more-container mt-5 row px-5 px-md-0"
+          >
+            {
+              vehicles.length > 0 &&
+                vehicles.map((el, i) => {
+                  const img = el.image || 'https://via.placeholder.com/261x333?text=Popular+in+town'
+                  return (
+                    <VehicleImage
+                      to={`/vehicles/${el.id}`}
+                      key={i}
+                      src={img}
+                      name={capitalize(el.name)}
+                      location={capitalize(el.location)}
+                      className='view-more-item p-0 pe-md-3 col-12 col-md-4 col-lg-3'
+                    />
+                  )
+                })  
+            }
           </div>
-        </main>
-      </Layout>
-    )
-  }
+          <p className={`no-items mt-5 justify-self-center align-self-center ${vehicles.length === 0 && 'h-100'} `}>There is no vehicle left</p>
+        </div>
+      </main>
+    </Layout>
+  )
 }
