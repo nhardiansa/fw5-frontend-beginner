b20d49f2d81e438a653814441fcd9481566e1ba6
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-24 22:17:03 +0800

    feat: getData helper

---
+++ src/helpers/http.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getData = async (uri) => {
+  try {
+    const {data} = await axios.get('http://localhost:5000' + uri)
+    if (data.success) {
+      return data;
+    }
+
+    data.results = []
+    return data;
+  } catch (error) {
+    console.log(error)
+    const data = {
+      results: [],
+    }
+    return data;
+  }
+}
