d8228dca0fb55913eaf499a934a0f2361ca6b498
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-24 11:58:36 +0800

    fix: restyle and change to Link

--- src/components/VehicleImage/VehicleImage.js
+++ src/components/VehicleImage/VehicleImage.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import style from './VehicleImage.module.css';
+import { Link } from 'react-router-dom';
 
 export default class VehicleImage extends Component {
   render() {
-    const { src, name, location, alt, className } = this.props
+    const { src, name, location, alt, className, to } = this.props
     return (
-      <div className={`${style.wrapper} ${className} position-relative`}>
+      <Link to={to} className={`${style.wrapper} ${className} position-relative`}>
         <img className={style.img} src={src} alt={alt || ''} srcSet="" />
         <div className={`${style.desc} position-absolute`}>
           <h3 className={style.name}>{name}</h3>
           <p className={style.location}>{location}</p>
         </div>
-      </div>
+      </Link>
     )
   }
 }
