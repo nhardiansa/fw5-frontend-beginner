b56d750936254932151181e8c4a2be1649000df5
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-24 22:17:34 +0800

    fix: change to Function Component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Footer from './Footer/Footer'
-import Navbar from './Navbar'
+import {Navbar} from './Navbar'
 
 export default class Layout extends Component {
   render() {
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import {Link} from 'react-router-dom'
 
 import logo from '../../assets/img/car-wheel.png'
@@ -6,119 +6,115 @@ import profilePict from '../../assets/img/profile-picture/samantha-doe.png'
 import msgIcon from '../../assets/img/msg-icon.svg'
 import './style.css';
 
-export default class Navbar extends Component {
-  state = {
-    isLogged: this.props.isLogged || false
-  }
+export const Navbar = () => {
+  const [isLogged, setIsLogged] = useState(false)
 
-  render() {
-    return (
-      <nav
-        className="navbar navbar-expand-lg navbar-light bg-white position-fixed top-0 w-100 py-lg-4"
-      >
-        <div className="container d-flex align-items-center">
-          <a className="navbar-brand" href="/">
-            <img
-              className="logo"
-              src={logo}
-              alt="vehicle-rent-logo"
-            />
-          </a>
-          <div className="d-flex">
-            {
-              this.state.isLogged && (
-                <div
-                  className="profile d-flex d-lg-none justify-content-between align-items-center"
-                >
-                  <a href="/profile.html" className="profile-img">
-                    <img
-                      src={profilePict}
-                      alt="profile-img"
-                      className="profile-pict rounded-circle"
-                    />
-                  </a>
-                  <div className="message-notif position-relative ms-4 me-2">
-                    <img
-                      src={msgIcon}
-                      alt="message"
-                      className="message"
-                    />
-                    <div
-                      className="count position-absolute rounded-circle d-flex justify-content-center align-items-center"
-                    >
-                      1
-                    </div>
-                  </div>
-                </div>
-              )
-            }
-            
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav d-flex ms-auto">
-              <Link to='/' className="nav-link" aria-current="page" >Home</Link>
-              <Link to='/vehicles' className="nav-link" >Vehicle Type</Link>
-              <Link to='/histories' className="nav-link active" >History</Link>
-              <Link to='/' className="nav-link" >About</Link>
-            </div>
-            {
-              this.state.isLogged && (
-                <div
-                  className="profile d-none d-lg-flex justify-content-between align-items-center"
-                >
-                  <div className="message-notif position-relative">
-                    <img
-                      src={msgIcon}
-                      alt="message"
-                      className="message"
-                    />
-                    <div
-                      className="count position-absolute rounded-circle d-flex justify-content-center align-items-center"
-                    >
-                      1
-                    </div>
-                  </div>
-                  <a href="/profile.html" className="profile-img">
-                    <img
-                      src={profilePict}
-                      alt="profile-img"
-                      className="profile-pict rounded-circle"
-                    />
-                  </a>
-                </div>
-              )
-            }
-            {
-              !this.state.isLogged && (
-                <div className="auth d-flex flex-column flex-lg-row">
-                  <Link to="/login"  className="login-btn fw-normal btn rounded-3 mb-3 mb-lg-0" href="/login.html">
-                    Login
-                  </Link>
-                  <Link
-                    to="/register"
-                    className="register-btn fw-normal btn rounded-3 mb-3 mb-lg-0"
-                    href="/signup.html"
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light bg-white position-fixed top-0 w-100 py-lg-4"
+    >
+      <div className="container d-flex align-items-center">
+        <a className="navbar-brand" href="/">
+          <img
+            className="logo"
+            src={logo}
+            alt="vehicle-rent-logo"
+          />
+        </a>
+        <div className="d-flex">
+          {
+            isLogged && (
+              <div
+                className="profile d-flex d-lg-none justify-content-between align-items-center"
+              >
+                <a href="/profile.html" className="profile-img">
+                  <img
+                    src={profilePict}
+                    alt="profile-img"
+                    className="profile-pict rounded-circle"
+                  />
+                </a>
+                <div className="message-notif position-relative ms-4 me-2">
+                  <img
+                    src={msgIcon}
+                    alt="message"
+                    className="message"
+                  />
+                  <div
+                    className="count position-absolute rounded-circle d-flex justify-content-center align-items-center"
                   >
-                    Register
-                  </Link>
+                    1
+                  </div>
                 </div>
-              )
-            }
-          </div>
+              </div>
+            )
+          }
+          
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
-      </nav>
-    )
-  }
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav d-flex ms-auto">
+            <Link to='/' className="nav-link" aria-current="page" >Home</Link>
+            <Link to='/vehicles' className="nav-link" >Vehicle Type</Link>
+            <Link to='/histories' className="nav-link active" >History</Link>
+            <Link to='/' className="nav-link" >About</Link>
+          </div>
+          {
+            isLogged && (
+              <div
+                className="profile d-none d-lg-flex justify-content-between align-items-center"
+              >
+                <div className="message-notif position-relative">
+                  <img
+                    src={msgIcon}
+                    alt="message"
+                    className="message"
+                  />
+                  <div
+                    className="count position-absolute rounded-circle d-flex justify-content-center align-items-center"
+                  >
+                    1
+                  </div>
+                </div>
+                <a href="/profile.html" className="profile-img">
+                  <img
+                    src={profilePict}
+                    alt="profile-img"
+                    className="profile-pict rounded-circle"
+                  />
+                </a>
+              </div>
+            )
+          }
+          {
+            !isLogged && (
+              <div className="auth d-flex flex-column flex-lg-row">
+                <Link to="/login"  className="login-btn fw-normal btn rounded-3 mb-3 mb-lg-0" href="/login.html">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="register-btn fw-normal btn rounded-3 mb-3 mb-lg-0"
+                  href="/signup.html"
+                >
+                  Register
+                </Link>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </nav>
+  )
 }
