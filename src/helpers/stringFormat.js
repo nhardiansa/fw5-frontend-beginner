c21ae8cca6dcc92ddb3e3701ff56e36b9b28e05e
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-05 23:08:19 +0800

    feat: date formatter

--- src/helpers/stringFormat.js
+++ src/helpers/stringFormat.js
@@ -14,3 +14,14 @@ export const queryFormat = (query) => {
 export const capitalize = (str) => {
   return str.split(' ').map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(' ');
 };
+
+export const dateFormatter = (date, input = false) => {
+  if (input) {
+    // console.log(date.split('-').reverse().join('/'), 'frontend');
+    return date.split('-').reverse().join('/');
+  }
+  let result = new Date(date).toLocaleString('id-ID').split(' ')[0];
+  result = result.split(/\D+/g).reverse().map(el => el.length === 1 ? `0${el}` : el).join('-');
+  // console.log(result, 'backend');
+  return result;
+};
