66c44caa53288f521ff9c712b2b54285d18c4912
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-07 13:20:52 +0800

    feat: custom alert

---
+++ src/helpers/alert.js
@@ -0,0 +1,10 @@
+import Swal from 'sweetalert2';
+
+export default function customAlert (message, type) {
+  Swal.fire({
+    icon: 'error',
+    text: 'Do you want to continue',
+    title: 'Error',
+    confirmButtonText: 'Cool'
+  });
+}
