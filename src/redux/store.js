dbc352a5313e31546823c87d183f53f6656eb0b1
Nabil Hardiansa <nhardiansa@gmail.com>
2022-04-28 14:09:02 +0800

    feat: configure redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise-middleware';
-// import logger from 'redux-logger';
+/* eslint-disable comma-dangle */
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise-middleware";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+// import thunk from "redux-thunk";
 
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
-export default createStore(
-  rootReducer,
-  applyMiddleware(
-    promise
-    // logger
-  )
-);
+// export default createStore(
+//   rootReducer,
+//   applyMiddleware(
+//     promise
+//     // logger
+//   )
+// );
+
+export default () => {
+  const store = createStore(rootReducer, applyMiddleware(promise, logger));
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
